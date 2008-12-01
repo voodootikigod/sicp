0f970cb8f725b6824eb1a10a71468b941325ad1c
Dennis Marti <dennis_marti@yahoo.com>
2008-12-01 16:49:18 -0500

    exercise 2.8 (and 2.7 out of necessity)

--- chap-2/evens.js
+++ chap-2/evens.js
@@ -1,9 +1,10 @@
+// chapter 2, even exercises
 // started on Monday, December  1, 2008
 // $Id$
 
-// chapter 2, even exercises
 
 // exercise 2.2, page 89
+
 function make_point (a,b) {return cons(a,b);}
 function x_point (s) {return car(s);}
 function y_point (s) {return cdr(s);}
@@ -38,7 +39,10 @@ function test_midpoint_segment () {
     }
 }
 
+
 // exercise 2.4, page 92
+// cons, car, cdr as higher order functions
+
 function cons (x,y) {
     return function (m) {return m(x,y);};
 }
@@ -51,3 +55,24 @@ function cdr (z) {
     return z(function (p,q) {return q;});
 }
 
+
+// exercise 2.6, page 93
+// church numerals
+
+// exercise 2.8, page 95
+// interval arithmetic
+
+function make_interval(a,b) {return cons(a,b);}
+// oops, 2.7 for free...
+function lower_bound(i) {return car(i);}
+function upper_bound(i) {return cdr(i);}
+
+function add_interval(a,b) {
+    return make_interval( lower_bound(a) + lower_bound(b),
+			  upper_bound(a) + upper_bound(b));
+}
+
+function sub_interval (a,b) {
+    return make_interval( lower_bound(a) - lower_bound(b),
+			  upper_bound(a) - upper_bound(b));
+}
